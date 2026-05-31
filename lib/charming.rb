408baa6f6cb53f8b7c86b0baaff437f63bc7ec91
Pando <pando@Pandos-MacBook-Pro.local>
2026-05-31 07:15:36 -0400

    Add Charming entrypoint and version

---
+++ lib/charming.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "zeitwerk"
+
+loader = Zeitwerk::Loader.for_gem
+loader.inflector.inflect(
+  "cli" => "CLI",
+  "ui" => "UI",
+  "tty_backend" => "TTYBackend"
+)
+loader.setup
+
+module Charming
+  class Error < StandardError; end
+
+  def self.run(application, backend: nil)
+    Runtime.new(application, backend: backend).run
+  end
+
+  def self.key_of(event)
+    key = event.respond_to?(:key) ? event.key : event
+    key.to_sym
+  end
+end
---
+++ lib/charming/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Charming
+  VERSION = "0.1.0"
+end
